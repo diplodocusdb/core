023a41a443eb496e440157ed0a97c868f9eb44cb
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-12-25 15:38:03 +0000

    Add Visual Studio 2019 files and rename some classes (#7)

--- include/DiplodocusDB/Core/DataTypeModifier.h
+++ include/DiplodocusDB/Core/DataTypeModifier.h
@@ -1,24 +1,9 @@
 /*
-    Copyright (c) 2018 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Copyright (c) 2018-2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/diplodocusdb/core/blob/main/LICENSE.txt
 */
+
 
 #ifndef _DIPLODOCUSDB_CORE_DATATYPEMODIFIER_H_
 #define _DIPLODOCUSDB_CORE_DATATYPEMODIFIER_H_
@@ -26,10 +11,10 @@
 namespace DiplodocusDB
 {
 
-enum class EDataTypeModifier
+enum class DataTypeModifier
 {
-    eNone = 0,
-    eList = 1
+    none = 0,
+    list = 1
 };
 
 }
--- include/DiplodocusDB/Core/PrimitiveDataType.h
+++ include/DiplodocusDB/Core/PrimitiveDataType.h
@@ -1,23 +1,7 @@
 /*
-    Copyright (c) 2017-2019 Xavier Leclercq
-
-    Permission is hereby granted, free of charge, to any person obtaining a
-    copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-    IN THE SOFTWARE.
+    Copyright (c) 2017-2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/diplodocusdb/core/blob/main/LICENSE.txt
 */
 
 #ifndef _DIPLODOCUSDB_CORE_PRIMITIVEDATATYPE_H_
@@ -26,23 +10,36 @@
 namespace DiplodocusDB
 {
 
-enum class EPrimitiveDataType
+enum class PrimitiveDataType
 {
-    eNULL = 0,
-    eBinary = 0x01,
-    eBoolean = 0x02,
-    eUTF8String = 0x03,
-    eInt8 = 0x10,
-    eInt16 = 0x11,
-    eInt32 = 0x12,
-    eInt64 = 0x13,
-    eInt = 0x17,
-    eUnsignedInt8 = 0x18,
-    eUnsignedInt16 = 0x19,
-    eUnsignedInt32 = 0x1A,
-    eUnsignedInt64 = 0x1B,
-    eUnsignedInt = 0x1F,
-    eUTCDateTime = 0x20
+    null = 0x0,
+    binary = 0x01,
+    boolean = 0x02,
+
+    int8bit = 0x20,
+    int16bit = 0x21,
+    int32bit = 0x22,
+    int64bit = 0x23,
+    intInfinite = 0x3F,
+
+    unsignedInt8bit = 0x40,
+    unsignedInt16bit = 0x41,
+    unsignedInt32bit = 0x42,
+    unsignedInt64bit = 0x43,
+    unsignedIntInfinite = 0x5F,
+
+    IEEE754Binary16 = 0x60,
+    IEEE754Binary32 = 0x61,
+    IEEE754Binary64 = 0x62,
+    IEEE754Binary128 = 0x63,
+    IEEE754Binary256 = 0x64,
+
+    unicodeString = 0x80,
+
+    localDate = 0xA0,
+    localDateTime = 0xA1,
+    universalDateTime = 0xA2,
+    timeOfDay = 0xA3,
 };
 
 }
